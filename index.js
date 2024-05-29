b68bc4d191c3cbf8203dbab719980c6d566f6854
Mostafa <mostafa.hafez.de@gmail.com>
2024-05-29 11:07:12 +0200

    cors config

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ import routerComments from './src/modules/comments/comment.route.js';
 const app = express();
 const port =process.env.APP_PORT || 3000;
 
+const corsConfig = {
+    origin: "*", Credential:true, methods:["GET","POST","PATCH","DELETE"]
+}
 app.use(express())
 app.use(express.json());
-app.use(cors())
+app.use(cors(corsConfig))
 connectionDB()
 app.use("/users", routerUser)
 app.use("/posts", routerPost)
